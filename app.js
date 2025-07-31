ccd8fe9673c77e983d7ce9ae109adcb1e0307330
ALEXIS <ing.parradiaz@gmail.com>
2025-07-30 20:48:24 -0600

    agrega amigo a la lista

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+function agregarAmigo (){
+    //creamos una variable auxiliar para guardar el valor dentro del input
+    //usaremos .value para obtener el valor ingresado
+    let nuevoAmigoTexto = document.getElementById("amigo").value;
+
+    //verificaremos que el campo no este vacio
+    if (nuevoAmigoTexto === ""){
+        alert("Por favor, ingrese un nombre");
+        return;
+    }
+    
+    // llamamos lista
+    let lista = document.getElementById("listaAmigos");
+    let ElementoAmigo = document.createElement("li");
+    ElementoAmigo.textContent = nuevoAmigoTexto;
+    lista.appendChild(ElementoAmigo);
+    document.getElementById("amigo").value = "";
+
+    console.log(ElementoAmigo.textContent);
+
+
+
+}
+
+
+
